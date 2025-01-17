eca8b6070d6be3d42ff7ef5947f0e6c3b4c0d54c
avishi1416 <mehrotraavishi@gmail.com>
2025-01-17 19:24:35 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,113 @@
+
+EXPERIMENT 1
+
+1.
+print("Hello Everyone!!!")
+2.
+print("Hello\nWorld")
+3.
+print("Hello\n     world")
+4.
+print("Rohit's date of birth is 12\05\1999")
+5.
+x="Hello"
+print(x)
+6.
+a=1416
+print(type(a))
+b="welcome to python"
+print(type(b))
+
+7.
+a=input("enter the first string")
+b=input("enter the second string")
+c=a+b
+print(c)
+
+8.
+x="Avishi"
+y="(avi)"
+z="Mehrotra"
+a=x+y+z
+print(a)
+
+9.
+a=input("NAME:")
+b=input("SAP ID:")
+c=input("DOB:")
+d=input("ADDRESS:")
+e=input("PROGRAMME:")
+f=input("SEMESTER:")
+
+Experiment 2
+1.
+x=9
+y=7
+print(x+y) 
+print(x-y)
+print(x*y)
+print(x/y)
+
+
+
+2.
+radius = int(input("Enter the radius of the circle: "))
+area = 3.14 * radius ** 2
+print("The area of the circle is: {area:}")
+
+3.
+x = 4
+y = 3
+result = (x + y) ** 2
+print("The result of (x + y) * (x + y) is: {result}")
+
+4.a = int(input("Enter side a: "))
+b = int(input("Enter side b: "))
+c = (a**2 + b**2)
+print("The length of the hypotenuse is: {c:}")
+
+5.
+principal = int(input("Enter the principal amount: "))
+rate = int(input("Enter the rate of interest (in %): "))
+time = int(input("Enter the time (in years): "))
+simple_interest = (principal * rate * time) / 100
+print("The simple interest is: {simple_interest:}")
+
+6.
+a = int(input("Enter side a: "))
+b = int(input("Enter side b: "))
+area=a*b/2
+print("The area of the triangle is: {area:}")
+
+7.
+seconds = int(input("Enter the seconds: "))
+hours = seconds // 3600
+minutes = (seconds % 3600) // 60
+seconds = seconds % 60
+print("{seconds} seconds is {hours} hours, {minutes} minutes, and {seconds} seconds.")
+
+8.
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+a, b = b, a
+print("After swapping: a = {a}, b = {b}")
+
+9.
+n = int(input("Enter a positive integer n: "))
+sum = n * (n + 1) // 2
+print("The sum of the first n natural numbers is: {sum}")
+
+10.
+print("Truth Table for Bitwise Operators")
+print(" A&B  A|B  A^B")
+for a in [0, 1]:
+    for b in [0, 1]:
+        print("  {a & b}    {a | b}    {a ^ b}")
+
+11.
+
+
+
+
+
+
